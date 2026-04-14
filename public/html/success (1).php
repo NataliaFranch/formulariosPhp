eff6395532d0db5f73a598c9f698d3df427a3e43
NataliaFranch <36162379+NataliaFranch@users.noreply.github.com>
2026-04-14 13:14:56 +0200

    Add files via upload

---
+++ public/html/success (1).php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" 
+		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" 
+		crossorigin="anonymous">
+	<title>Exito</title>
+</head>
+<body>
+	<div class="container-fluid" align="center">
+		<h1> Fuiste registrado con éxito </h1>
+	</div>
+</body>
+</html>
